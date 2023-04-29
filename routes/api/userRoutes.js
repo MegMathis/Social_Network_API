0ebc26fa20cc62a3c6fb64359359b5a3c8a6f4a4
MegMathis <mcdonough.megan25@gmail.com>
2023-04-29 19:56:12 -0400

    userRoutes completed

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+// get all users
+// get single user by its _id and populated thought and friend data
+// post a new user
+// put to update a user by its _id,
+// delete to remove user by its _id
+
+const router = require("express").Router;
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:id
+router.route("/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
