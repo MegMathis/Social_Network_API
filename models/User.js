c0725316695cb0ce9af8b689186a3fb00ba69926
MegMathis <mcdonough.megan25@gmail.com>
2023-04-28 21:39:17 -0400

    user model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,46 @@
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email"],
+    },
+    //   array of _id values referencing the THOUGHT model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    //   array of _id values referencing the User model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+\@.+\..+/, "Please enter a valid email"],
-  },
-  thoughs: {
-    type: Schema.Types.ObjectID,
-    ref: "Thought",
-  },
+  {
+    // indicating we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// create a virtual called friendCount that retrieves the length of the user's friends array field on query
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
+
+const User = model("user", UserSchema);
+
+module.exports = User;
