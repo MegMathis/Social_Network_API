e58e844f5fc8002442fb18c442ee7c579e3c5793
MegMathis <mcdonough.megan25@gmail.com>
2023-04-29 22:08:47 -0400

    using moment to try to format timestamps

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
+const moment = require("moment");
 
 const thoughtSchema = new Schema(
   {
@@ -12,7 +13,8 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      // figure out timestamp
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
       // use getter method to format the timestamp on query
     },
     //   user that created this thougth
