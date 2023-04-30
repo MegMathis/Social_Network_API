59ecbb3751540d271f51702f7f732e3a04bb7874
MegMathis <mcdonough.megan25@gmail.com>
2023-04-29 20:01:54 -0400

    thoughtRoutes completed

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,37 @@
+// get all thoughts
+// get to get a single thought by its _id
+// post to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// put to update a thought by its _id
+// delete to remove a thought by its _id
+// post to create a reaction stored in a single thought's reactions array field
+// delete to pull and remove a reaction by the reaction's reactionId value
+
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReation,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts - getAllThought and CreateThought
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id - getThoughtById, updateThought, and deleteThought
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions - addReaction
+router.route("/:thoughtId/reactions").post(addReation);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId - removeReaction
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
