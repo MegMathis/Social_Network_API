6588e61ff979e099a13abedda2110aca9a9a445b
MegMathis <mcdonough.megan25@gmail.com>
2023-04-30 11:22:16 -0400

    updated serverjs and userroutes

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log("API server funning on PORT " + PORT);
+    console.log("API server running on PORT " + PORT);
   });
 });
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -20,7 +20,7 @@ const {
 router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:id
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
